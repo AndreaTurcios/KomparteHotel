33c87773303077aa3f2623c4a4d2bce4bbd19691
AndreaTurcios <turciosbarquero@gmail.com>
2022-01-31 02:41:00 -0600

    Avances varios disenio

--- Komparte/Komparte/frmMain.Designer.cs
+++ Komparte/Komparte/frmMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Komparte
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,20 +29,75 @@ namespace Komparte
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panelMenuLateral = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelMenuLateral.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // panelMenuLateral
+            // 
+            this.panelMenuLateral.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelMenuLateral.Controls.Add(this.panelLogo);
+            this.panelMenuLateral.Controls.Add(this.btnMedia);
+            this.panelMenuLateral.Controls.Add(this.panel1);
+            this.panelMenuLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenuLateral.Location = new System.Drawing.Point(0, 0);
+            this.panelMenuLateral.Name = "panelMenuLateral";
+            this.panelMenuLateral.Size = new System.Drawing.Size(250, 896);
+            this.panelMenuLateral.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(3, 3);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(247, 158);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMedia.Location = new System.Drawing.Point(3, 167);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(250, 45);
+            this.btnMedia.TabIndex = 1;
+            this.btnMedia.Text = "Media";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 218);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 75);
+            this.panel1.TabIndex = 2;
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1054, 571);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1348, 896);
+            this.Controls.Add(this.panelMenuLateral);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelMenuLateral.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel panelMenuLateral;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button btnMedia;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
