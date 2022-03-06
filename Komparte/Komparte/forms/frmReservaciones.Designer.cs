898e88f450c61b5353783f033be8c1761c18e5df
AndreaTurcios <turciosbarquero@gmail.com>
2022-03-05 22:02:48 -0600

    reservaciones ordenando formulario

--- Komparte/Komparte/forms/frmReservaciones.Designer.cs
+++ Komparte/Komparte/forms/frmReservaciones.Designer.cs
@@ -54,6 +54,7 @@ namespace Komparte.forms.obj
             this.button7 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -62,6 +63,8 @@ namespace Komparte.forms.obj
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -75,8 +78,7 @@ namespace Komparte.forms.obj
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.groupBox5);
             this.groupBox1.Controls.Add(this.groupBox4);
             this.groupBox1.Controls.Add(this.groupBox3);
             this.groupBox1.Controls.Add(this.numericUpDown2);
@@ -87,10 +89,6 @@ namespace Komparte.forms.obj
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(14, 90);
             this.groupBox1.Name = "groupBox1";
@@ -121,7 +119,7 @@ namespace Komparte.forms.obj
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(226, 109);
+            this.label4.Location = new System.Drawing.Point(226, 90);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(135, 25);
             this.label4.TabIndex = 10;
@@ -130,7 +128,7 @@ namespace Komparte.forms.obj
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 62);
+            this.label2.Location = new System.Drawing.Point(29, 43);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 25);
             this.label2.TabIndex = 7;
@@ -138,7 +136,7 @@ namespace Komparte.forms.obj
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(877, 183);
+            this.button4.Location = new System.Drawing.Point(833, 30);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(111, 33);
             this.button4.TabIndex = 5;
@@ -147,7 +145,7 @@ namespace Komparte.forms.obj
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(599, 183);
+            this.button3.Location = new System.Drawing.Point(540, 30);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(111, 33);
             this.button3.TabIndex = 4;
@@ -156,7 +154,7 @@ namespace Komparte.forms.obj
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(276, 183);
+            this.button2.Location = new System.Drawing.Point(259, 30);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(111, 33);
             this.button2.TabIndex = 3;
@@ -165,7 +163,7 @@ namespace Komparte.forms.obj
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(29, 183);
+            this.button1.Location = new System.Drawing.Point(38, 30);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 33);
             this.button1.TabIndex = 2;
@@ -224,7 +222,7 @@ namespace Komparte.forms.obj
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(355, 49);
+            this.button6.Location = new System.Drawing.Point(355, 30);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(112, 34);
             this.button6.TabIndex = 17;
@@ -234,14 +232,14 @@ namespace Komparte.forms.obj
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(104, 53);
+            this.textBox1.Location = new System.Drawing.Point(104, 34);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(245, 31);
             this.textBox1.TabIndex = 18;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(384, 103);
+            this.numericUpDown1.Location = new System.Drawing.Point(384, 84);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(83, 31);
             this.numericUpDown1.TabIndex = 19;
@@ -249,7 +247,7 @@ namespace Komparte.forms.obj
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(29, 109);
+            this.label7.Location = new System.Drawing.Point(29, 90);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(63, 25);
             this.label7.TabIndex = 20;
@@ -257,7 +255,7 @@ namespace Komparte.forms.obj
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(104, 103);
+            this.numericUpDown2.Location = new System.Drawing.Point(104, 84);
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(95, 31);
             this.numericUpDown2.TabIndex = 21;
@@ -265,7 +263,7 @@ namespace Komparte.forms.obj
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(545, 61);
+            this.checkBox1.Location = new System.Drawing.Point(50, 30);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(105, 29);
             this.checkBox1.TabIndex = 7;
@@ -275,7 +273,7 @@ namespace Komparte.forms.obj
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(545, 103);
+            this.checkBox2.Location = new System.Drawing.Point(50, 73);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(120, 29);
             this.checkBox2.TabIndex = 24;
@@ -294,7 +292,7 @@ namespace Komparte.forms.obj
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.comboBox1);
-            this.groupBox3.Location = new System.Drawing.Point(753, 30);
+            this.groupBox3.Location = new System.Drawing.Point(753, 16);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(249, 121);
             this.groupBox3.TabIndex = 25;
@@ -303,12 +301,27 @@ namespace Komparte.forms.obj
             // 
             // groupBox4
             // 
-            this.groupBox4.Location = new System.Drawing.Point(510, 30);
+            this.groupBox4.Controls.Add(this.checkBox1);
+            this.groupBox4.Controls.Add(this.checkBox2);
+            this.groupBox4.Location = new System.Drawing.Point(510, 18);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(237, 121);
             this.groupBox4.TabIndex = 13;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Check";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button4);
+            this.groupBox5.Controls.Add(this.button1);
+            this.groupBox5.Controls.Add(this.button3);
+            this.groupBox5.Controls.Add(this.button2);
+            this.groupBox5.Location = new System.Drawing.Point(29, 140);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(973, 77);
+            this.groupBox5.TabIndex = 26;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Controles";
             // 
             // frmReservaciones
             // 
@@ -329,6 +342,9 @@ namespace Komparte.forms.obj
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -360,5 +376,6 @@ namespace Komparte.forms.obj
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox5;
     }
 }
