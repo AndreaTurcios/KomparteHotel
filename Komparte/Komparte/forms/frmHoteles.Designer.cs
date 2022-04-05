a4f493318f594bfe721f7cb51c0febed83ec4328
AndreaTurcios <turciosbarquero@gmail.com>
2022-04-05 08:39:26 -0600

    FRMhOTELES

--- Komparte/Komparte/forms/frmHoteles.Designer.cs
+++ Komparte/Komparte/forms/frmHoteles.Designer.cs
@@ -32,6 +32,7 @@ namespace Komparte.forms
             this.panel2 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.GestionHotel = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -40,10 +41,13 @@ namespace Komparte.forms
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button5 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.Actividad = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.GestionHotel.SuspendLayout();
             this.groupBox5.SuspendLayout();
@@ -74,12 +78,16 @@ namespace Komparte.forms
             // 
             // GestionHotel
             // 
+            this.GestionHotel.Controls.Add(this.textBox3);
+            this.GestionHotel.Controls.Add(this.label5);
+            this.GestionHotel.Controls.Add(this.textBox2);
+            this.GestionHotel.Controls.Add(this.label4);
+            this.GestionHotel.Controls.Add(this.textBox1);
+            this.GestionHotel.Controls.Add(this.label2);
             this.GestionHotel.Controls.Add(this.comboBox1);
             this.GestionHotel.Controls.Add(this.groupBox5);
             this.GestionHotel.Controls.Add(this.groupBox2);
-            this.GestionHotel.Controls.Add(this.label2);
             this.GestionHotel.Controls.Add(this.label1);
-            this.GestionHotel.Controls.Add(this.Actividad);
             this.GestionHotel.Location = new System.Drawing.Point(15, 91);
             this.GestionHotel.Margin = new System.Windows.Forms.Padding(4);
             this.GestionHotel.Name = "GestionHotel";
@@ -88,6 +96,20 @@ namespace Komparte.forms
             this.GestionHotel.TabIndex = 1;
             this.GestionHotel.TabStop = false;
             this.GestionHotel.Text = "Gestion Hotel";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tipo Hotel 1",
+            "Tipo Hotel 2",
+            "Tipo Hotel 3"});
+            this.comboBox1.Location = new System.Drawing.Point(120, 61);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 28);
+            this.comboBox1.TabIndex = 30;
+            this.comboBox1.Text = "Tipo Hotel";
             // 
             // groupBox5
             // 
@@ -178,50 +200,69 @@ namespace Komparte.forms
             this.dataGridView1.Size = new System.Drawing.Size(1107, 232);
             this.dataGridView1.TabIndex = 4;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(455, 63);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Actividad";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 63);
+            this.label1.Location = new System.Drawing.Point(17, 69);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Tipo Hotel";
             // 
-            // Actividad
+            // textBox1
             // 
-            this.Actividad.FormattingEnabled = true;
-            this.Actividad.Location = new System.Drawing.Point(588, 52);
-            this.Actividad.Margin = new System.Windows.Forms.Padding(4);
-            this.Actividad.Name = "Actividad";
-            this.Actividad.Size = new System.Drawing.Size(179, 28);
-            this.Actividad.TabIndex = 0;
-            this.Actividad.Text = "Actividad";
-            this.Actividad.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.textBox1.Location = new System.Drawing.Point(433, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 26);
+            this.textBox1.TabIndex = 31;
             // 
-            // comboBox1
+            // label2
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Tipo Hotel 1",
-            "Tipo Hotel 2",
-            "Tipo Hotel 3"});
-            this.comboBox1.Location = new System.Drawing.Point(120, 55);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(179, 28);
-            this.comboBox1.TabIndex = 30;
-            this.comboBox1.Text = "Tipo Hotel";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Encargado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(756, 35);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 30);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Teléfono:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(859, 29);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(259, 26);
+            this.textBox2.TabIndex = 34;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(756, 83);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 30);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Dirección:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(859, 87);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(259, 26);
+            this.textBox3.TabIndex = 36;
             // 
             // frmHoteles
             // 
@@ -250,9 +291,7 @@ namespace Komparte.forms
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.GroupBox GestionHotel;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox Actividad;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button1;
@@ -262,5 +301,11 @@ namespace Komparte.forms
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
